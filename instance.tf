254ec9570145a097a4a76dc8bb9ec012781f8404
Muhammad Hassaan <hassaan.solves@gmail.com>
2025-04-01 23:39:10 +0500

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}                                       
